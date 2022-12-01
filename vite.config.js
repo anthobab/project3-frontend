997a5b7cd2865b2d2f34b1688075f3b1cca489c3
anthobab <anthony.babin1@gmail.com>
2022-12-01 19:23:28 +0100

    vite-config & api add/v1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'; // Vite doesn't load .env by default
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log(env.PORT);
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    plugins: [react()],
+    server: {
+      port: env.PORT,
+    },
+    // preview: {
+    //   port: env.PREVIEW_PORT,
+    // },
+  };
+});
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: import.meta.env.PORT,
+//   },
+//   preview: {
+//     port: import.meta.env.PREVIEW_PORT,
+//   },
+// });
