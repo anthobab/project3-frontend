61083d7af328e4a3501aabc9baf2fad8425de872
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-12-05 10:50:07 +0100

    Added font awesome icon and some style

--- src/components/Nav/NavMain.jsx
+++ src/components/Nav/NavMain.jsx
@@ -1,26 +1,40 @@
 import { NavLink } from 'react-router-dom';
 import useAuth from '../../auth/useAuth';
-import '../../styles/NavMain.css';
+import './NavMain.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt, faMap, faUser, faBellConcierge, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 const NavMain = () => {
-  const { isLoggedIn, currentUser, removeUser } = useAuth();
+  const { isLoggedIn } = useAuth();
   return (
     <nav className="NavMain">
       <NavLink className="logo" to="/">
-        Court Circuits
+        <FontAwesomeIcon icon={faBolt} style />
       </NavLink>
-      <NavLink to="/map">Map</NavLink>
+      <NavLink to="/map">
+        <FontAwesomeIcon icon={faMap} />
+      </NavLink>
       {isLoggedIn && (
         <>
-          <NavLink to="/profile">{currentUser && currentUser.email}</NavLink>
-          <NavLink to="/myservices">My Services</NavLink>
-          <button onClick={removeUser}>Log-Out</button>
+          <NavLink to="/myservices">
+            <FontAwesomeIcon icon={faBellConcierge} />
+          </NavLink>
+          <NavLink to="/profile">
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>
+          {/* <NavLink onClick={removeUser}>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+          </NavLink> */}
+          {/* <button onClick={removeUser}>Log-Out</button> */}
         </>
       )}
       {!isLoggedIn && (
         <>
-          <NavLink to="/signin">Log in</NavLink>
-          <NavLink to="/signup">Sign Up</NavLink>
+          <NavLink to="/signin">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+          </NavLink>
+          {/* <NavLink to="/signin">Log in</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink> */}
         </>
       )}
     </nav>
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,14 @@
 import FormSignIn from '../components/Forms/FormSignIn';
+import FormSignUp from '../components/Forms/FormSignUp';
 
 const Signin = () => {
-  return <FormSignIn />;
+  return (
+    <>
+      <FormSignIn />
+      <p>Alredy have an account?</p>
+      <FormSignUp />
+    </>
+  );
 };
 
 export default Signin;
